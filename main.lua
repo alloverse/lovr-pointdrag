e1ab611935c3f5a05085ea7a0d19519d42536ba9
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-08-26 10:53:25 +0200

    Move things in a scenegraph!

--- main.lua
+++ main.lua
@@ -20,43 +20,84 @@ local font = lovr.graphics.newFont(32)
 
 -- box and moving
 
-box = {
-  transform = lovr.math.newMat4():translate(-0.25, 1.5, -1.25),
-  size = lovr.math.newVec3(0.15, 0.25, 0.40),
-  offset = lovr.math.newMat4()
+Box = {
+  selected = false,
+  parent = nil
 }
 
-function box:draw()
+function Box:new(x, y, z, w, h, d)
+  o = {}
+  setmetatable(o, self)
+  self.__index = self
+  o.transform = lovr.math.newMat4():translate(x, y, z)
+  o.size = lovr.math.newVec3(w, h, d)
+  o.offset = lovr.math.newMat4()
+  o.collider = letters.world:newBoxCollider(0, 0, 0, o.size.x, o.size.y, o.size.z)
+  o.collider:setUserData(o)
+  o.children = {}
+  return o
+end
+
+function Box:addChild(c)
+  table.insert(self.children, c)
+  c.parent = self
+end
+
+function Box:transformInWorld()
+  if self.parent then
+    return lovr.math.mat4(self.parent:transformInWorld()):mul(self.transform)
+  else
+    return lovr.math.mat4(self.transform)
+  end
+end
+
+function convert(m, a, b)
+  local worldFromA = a and a:transformInWorld() or lovr.math.mat4()
+  local worldFromB = b and b:transformInWorld() or lovr.math.mat4()
+  local bFromWorld = worldFromB:invert()
+  local bFromA = bFromWorld * worldFromA
+  return bFromA * m
+end
+
+function Box:draw()
   lovr.graphics.setColor(self.selected and 0.9 or 0.7, 0.7, self.selected and 0.7 or 0.9)
   lovr.graphics.push()
   lovr.graphics.transform(self.transform)
   lovr.graphics.box('fill', 0, 0, 0, self.size)
-  
-  lovr.graphics.cylinder(0, self.size.y, -self.size.z/2, self.size.y, 3.14/2, 1, 0, 0, 0.0, 0.1)
+
+  for _, child in ipairs(self.children) do
+    child:draw()
+  end
   lovr.graphics.pop()
 end
 
-function box:select(hand)
+function Box:select(hand)
   self:deselect(self.heldBy) -- if held by another hand
   self.selected = true
   self.heldBy = hand
   local handTransform = lovr.math.mat4(lovr.headset.getPose(hand.device))
-  self.offset:set(handTransform:invert()):mul(self.transform)
+  self.offset:set(handTransform:invert()):mul(self:transformInWorld())
 end
-function box:deselect(hand)
+function Box:deselect(hand)
   if self.heldBy == hand then
     self.selected = false
     self.heldBy = nil
   end
 end
-function box:update()
+function Box:update()
   if self.heldBy then
     stickX, stickY = lovr.headset.getAxis(self.heldBy.device, "thumbstick")
     local handTransform = lovr.math.mat4(lovr.headset.getPose(self.heldBy.device))
-    self.transform:set(handTransform):mul(self.offset)
+    local newInWorld = handTransform:mul(self.offset)
+    local newInLocal = convert(newInWorld, nil, self.parent)
+    self.transform:set(newInLocal)
   end
-  local x, y, z, w, h, d, a, ax, ay, az = self.transform:unpack()
+  local x, y, z, w, h, d, a, ax, ay, az = self:transformInWorld():unpack()
   self.collider:setPose(x, y, z, a, ax, ay, az)
+
+  for _, child in ipairs(self.children) do
+    child:update()
+  end
 end
 
 -- grid
@@ -99,19 +140,23 @@ end
 -- global
 function lovr.load()
   letters.load()
-  box.collider = letters.world:newBoxCollider(0, 0, 0, box.size.x, box.size.y, box.size.z)
-  box.collider:setUserData(box)
-
   shader = lovr.graphics.newShader('standard')
-
   lovr.graphics.setBackgroundColor(0.95, 0.98, 0.98)
+
+  local box = Box:new(-0.25, 1.5, -1.25, 0.15, 0.25, 0.40)
+  local head = Box:new(0, 0.15, -0.1,   0.1, 0.1, 0.1)
+  local thingie = Box:new(1, 0, 0,   0.05, 0.3, 0.1)
+  table.insert(drawables, box)
+  box:addChild(head)
+  head:addChild(thingie)
+
   
   for i, hand in ipairs(letters.hands) do
     table.insert(drawables, hand)
   end
-  table.insert(drawables, box)
   grid:load()
   table.insert(drawables, grid)
+
 end
 
 function lovr.update()
@@ -128,7 +173,7 @@ function lovr.draw()
   lovr.graphics.setShader(shader)
 
   lovr.graphics.setColor(1,1,1)
-  lovr.graphics.box("fill", 0,0,0, 0.1, 0.1, 0.1)
+  lovr.graphics.sphere(0,0,0, 0.1)
 
   letters.draw()
   for i, thing in ipairs(drawables) do
@@ -137,7 +182,7 @@ function lovr.draw()
 
   -- debug draw colliders
   lovr.graphics.setColor(0, 1, 0, 1)
-  for _, collider in ipairs({box.collider}) do
+  for _, collider in ipairs(letters.world:getColliders()) do
     local x, y, z, a, ax, ay, az = collider:getPose()
     local w, h, d = collider:getShapes()[1]:getDimensions()
     lovr.graphics.box("line", x, y, z, w, h, d, a, ax, ay, az)
